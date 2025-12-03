7173a66c709ec11447606cc517636adc9a0d7c42
manodya <nadunimanodya@gmail.com>
2025-12-03 16:27:13 +0530

    Initial commit: Energy prediction project with CNN-LSTM model

---
+++ template.sh
@@ -0,0 +1,11 @@
+#creating folders
+mkdir data notebooks models reports src 
+#mkdir notebooks
+
+$creating files
+mkdir data/raw_data data/processed_data
+touch reports/report.pdf
+touch src/data_pre_processing.py src/feature_engineering.py src/baseline_model.py src/deep_model.py src/train.py
+touch notebooks/eda.ipynb notebooks/features.ipynb notebooks/model.ipynb
+
+
